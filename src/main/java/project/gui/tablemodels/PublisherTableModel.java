537aa1f95ada791f535fda8302d6198761cff6c1
Lukasz <uplukaszp1@gmail.com>
2018-01-11 12:36:02 +0100

    Added publisherTableModel

---
+++ src/main/java/project/gui/tablemodels/PublisherTableModel.java
@@ -0,0 +1,74 @@
+package project.gui.tablemodels;
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import project.model.Publisher;
+import project.repositories.PublisherRepository;
+
+public class PublisherTableModel extends AbstractTableModel {
+	
+	private PublisherRepository repo;
+	private List<Publisher> list;
+	
+	@Autowired
+	public PublisherTableModel() {
+		this.repo=repo;
+	}
+	public int getRowCount() {
+		
+		return list.size();
+	}
+
+	public int getColumnCount() {
+		
+		return 7;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		switch (columnIndex) {
+		case 0:
+			return list.get(rowIndex).getName();
+		case 1:
+			return list.get(rowIndex).getAddress();
+		case 2:
+			return list.get(rowIndex).getCity();
+		case 3:
+			return list.get(rowIndex).getZipCode();
+		case 4:
+			return list.get(rowIndex).getPhoneNumber();
+		case 5:
+			return list.get(rowIndex).getEmail();
+		default:
+			return "";
+		}
+	}
+	public void update()
+	{
+		list=repo.getall();
+	}
+	
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+			return "Nazwa";
+		case 1:
+			return "Adres";
+		case 2:
+			return "Miejscowoœæ";
+		case 3:
+			return "Kod pocztowy";
+		case 4:
+			return "Telefon";
+		case 5:
+			return "Email";
+		default:
+			return "";
+		}
+	}
+}
