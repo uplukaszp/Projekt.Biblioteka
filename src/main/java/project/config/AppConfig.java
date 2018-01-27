e4f5d5d26afee95754a22f21cca4a27a22ea6525
Lukasz <uplukaszp1@gmail.com>
2018-01-27 18:19:26 +0100

    Collation fix

--- src/main/java/project/config/AppConfig.java
+++ src/main/java/project/config/AppConfig.java
@@ -1,5 +1,7 @@
 package project.config;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
 
@@ -29,18 +32,21 @@ public class AppConfig {
 
 	@Bean
 	public DataSource dataSource() {
-
-		MysqlDataSource dataSource = new MysqlDataSource();
-		dataSource.setDatabaseName(dbName);
-		dataSource.setServerName(url);
-		dataSource.setUser(username);
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://"+url+":"+port+"/"+dbName);
+		dataSource.setUsername(username);
 		dataSource.setPassword(password);
-		dataSource.setPort(Integer.valueOf(port));
+		Properties p=new Properties();
+		p.setProperty("useUnicode", "yes");
+		p.setProperty("characterEncoding", "utf8");
+		dataSource.setConnectionProperties( p);
 		return dataSource;
 	}
 
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
+		
 		return new JdbcTemplate(dataSource());
 	}
 
